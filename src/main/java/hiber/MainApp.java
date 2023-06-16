6cf7be983eb90d613af0e7b0e27ab28765c701f9
mac <viktoriyya.com@yandex.ru>
2023-06-16 12:04:27 +0500

    В pom добавила postgres, в dbproperties прописала postgres базу данных, добавила класс Car связала @OneToOne с User, в AppConfig добавила его в setAnnotatedClasses, в UserDao добавила getUserByCar, реализовала его в UserDaoImp

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,15 +21,20 @@ public class MainApp {
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("lada", 9)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("mazda", 6)));
+
       List<User> users = userService.listUsers();
       for (User user : users) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
 
+      System.out.println(userService.getUserByCar("mazda", 6));
       context.close();
    }
 }
